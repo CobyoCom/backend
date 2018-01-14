8d91e7a3f63211c279982b4572a096cb281df087
sw31 <alexwhang94@gmail.com>
2018-01-14 17:33:53 -0500

    error codes

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const sqlite = require("sql.js");
 
 const db = new sqlite.Database();
 
+function perror(req, res, msg, ecode=500) {
+//  var ecode = typeof ecode !== 'undefined'? ecode : 500;
+  console.error("%s %s %s", req.method, req.originalUrl, msg);
+  res.sendStatus(ecode)
+}
+
+
 db.run(`
   CREATE TABLE Events (
     eventId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -37,8 +44,7 @@ app.post("/api/events", (req, res) => {
   const placeId = req.body.placeId;
   const eventTime = req.body.eventTime;
   if (!placeId || !eventTime) {
-    console.log("POST /api/events missing placeId or eventTime in body");
-    res.json([]);
+    perror(req, res, "missing placeId or eventTime in body");
     return;
   }
   const r = db.exec(`
@@ -47,33 +53,37 @@ app.post("/api/events", (req, res) => {
   `);
   
   if (!r || !r[0] || !r[0].values || r[0].values[0].length != 1) {
-    console.log("%s %s select last_insert_rowid() failed", req.method, req.originalUrl);
-    res.json([]);
+    perror(req, res, "last_insert_rowid() failed");
     return;
   }
   const resMap = {eventId: r[0].values[0][0]};
-  console.log("%s %s response %s", req.method, req.originalUrl, JSON.stringify(resMap));
+  console.log("%s %s -> %s", req.method, req.originalUrl, JSON.stringify(resMap));
   res.json(resMap);
 });
 
 app.get("/api/events/:eventId", (req, res) => { 
-  const eventIdString = req.params.eventId;
-  if (!eventIdString || !eventIdString.match(/^[0-9]+$/)) {
-    console.log("%s %s eventId missing or not positive integer", req.method, req.originalUrl);
-    res.json([]); 
+  if (!req.params.eventId) {
+    perror(req, res, "eventId missing");
     return;
   }
-  const eventId = Number(eventIdString);
+  if (!req.params.eventId.match(/^[0-9]+$/)) {
+    perror(req, res, "eventId not positive integer", 404);
+    return;
+  }
+  const eventId = Number(req.params.eventId);
   const r = db.exec(`
       SELECT placeId, eventTime from Events where eventId=${eventId} limit 1
   `);
-  if (!r || !r[0] || !r[0].values || r[0].values[0].length != 2) {
-    console.log("%s %s select eventId=%d from Events failed", req.method, req.originalUrl, eventId); 
-    res.json([]); 
+  if (r && !r[0]) {
+    perror(req, res, "eventId doesn't exist", 404);
+    return;
+  }
+  if (!r || !r[0].values || !r[0].values[0] || r[0].values[0].length != 2) {
+    perror(req, res, "select eventId from Events table failed");
     return;
   }
   const resMap = {placeId: r[0].values[0][0], eventTime: r[0].values[0][1]};
-  console.log("%s %s response %s", req.method, req.originalUrl, JSON.stringify(resMap));
+  console.log("%s %s -> %s", req.method, req.originalUrl, JSON.stringify(resMap));
   res.json(resMap);
 });
 
