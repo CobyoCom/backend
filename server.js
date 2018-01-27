1a484979b766846f0e8c244f705c1dc3c886b09c
asdf <asdf@adf>
2018-01-27 14:39:26 -0500

    hi

--- server.js
+++ server.js
@@ -1,49 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const sqlite = require("sql.js");
-
-//const fs = require("fs");
-//const filebuffer = fs.readFileSync("db.sqlite3");
-//const db = new sqlite.Database(filebuffer);
-//if (process.env.NODE_ENV === "production") {app.use(express.static("client/build"))}
-
 const db = new sqlite.Database();
-
-function perror(req, res, msg, ecode=500) {
-  console.error("%s %s %s", req.method, req.originalUrl, msg);
-  res.sendStatus(ecode)
-}
-function logResult(req, res, resMap) {
-  console.log("%s %s -> %s", req.method, req.originalUrl, JSON.stringify(resMap));
-  res.json(resMap);
-}
-
-
-db.run(`
-  PRAGMA FOREIGN_KEYS = ON;
-  CREATE TABLE Events (
-    eventId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    placeId TEXT NOT NULL,
-    eventTime TEXT NOT NULL
-  );
-  CREATE TABLE EventUsers (
-    eventId INTEGER NOT NULL,
-    userName TEXT NOT NULL,
-    estimatedArrivalTime TEXT NOT NULL,
-    lastUpdatedTime TEXT NOT NULL,
-    PRIMARY KEY (eventId, userName),
-    FOREIGN KEY (eventId) REFERENCES Events (eventId) ON DELETE CASCADE
-  ) WITHOUT ROWID;
-  INSERT INTO Events (placeId, eventTime) VALUES ('ChIJ_TSYQfV544kRLjVKmCw9xD0', '2018-01-13 18:20');
-  INSERT INTO EventUsers (eventId, userName, estimatedArrivalTime, lastUpdatedTime)
-    VALUES (1, 'Josh', '2018-01-13 18:25', '2018-01-13 17:30');
-`);
-
 const app = express();
+
+dbSeed();
+
 app.use(bodyParser.json());
 app.set("port", process.env.PORT || 3001);
+app.post("/api/events", createEvent);
+app.get("/api/events/:eventId", getEvent);
+app.post("/api/events/:eventId", updateEvent) 
+app.listen(app.get("port"));
+console.log("Running localhost:%s/api", app.get("port"))
 
-app.post("/api/events", (req, res) => {
+function dbSeed() {
+  db.run(`
+    PRAGMA FOREIGN_KEYS = ON;
+    CREATE TABLE Events (
+      eventId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      placeId TEXT NOT NULL,
+      eventTime TEXT NOT NULL
+    );
+    CREATE TABLE EventUsers (
+      eventId INTEGER NOT NULL,
+      userName TEXT NOT NULL,
+      estimatedArrivalTime TEXT NOT NULL,
+      lastUpdatedTime TEXT NOT NULL,
+      PRIMARY KEY (eventId, userName),
+      FOREIGN KEY (eventId) REFERENCES Events (eventId) ON DELETE CASCADE
+    ) WITHOUT ROWID;
+    INSERT INTO Events (placeId, eventTime) VALUES ('ChIJ_TSYQfV544kRLjVKmCw9xD0', '2018-01-13 18:20');
+    INSERT INTO EventUsers (eventId, userName, estimatedArrivalTime, lastUpdatedTime)
+      VALUES (1, 'Josh', '2018-01-13 18:25', '2018-01-13 17:30');
+  `);
+}
+
+function createEvent(req, res) {
   if (!req.body.placeId || !req.body.eventTime) {
     perror(req, res, "missing placeId or eventTime in body");
     return;
@@ -60,9 +53,9 @@ app.post("/api/events", (req, res) => {
     return;
   }
   logResult(req, res, {eventId: r[0].values[0][0]});
-});
+}
 
-app.get("/api/events/:eventId", (req, res) => { 
+function getEvent(req, res) { 
   if (!req.params.eventId) {
     perror(req, res, "eventId missing");
     return;
@@ -73,7 +66,7 @@ app.get("/api/events/:eventId", (req, res) => {
   }
   const eventId = Number(req.params.eventId);
   const r = db.exec(`
-    SELECT placeId, eventTime from Events where eventId = ${eventId} limit 1
+    SELECT placeId, eventTime FROM Events WHERE eventId = ${eventId} LIMIT 1
   `);
   if (r && !r[0]) {
     perror(req, res, "eventId doesn't exist", 404);
@@ -84,9 +77,9 @@ app.get("/api/events/:eventId", (req, res) => {
     return;
   }
   logResult(req, res, {placeId: r[0].values[0][0], eventTime: r[0].values[0][1]});
-});
+}
 
-app.post("/api/events/:eventId", (req, res) => { 
+function updateEvent(req, res) {
   if (!req.params.eventId || !req.body.userName || 
       !req.body.estimatedArrivalTime || !req.body.lastUpdatedTime) {
     perror(req, res, "eventId or userName or estimatedArrivalTime or lastUpdatedTime missing");
@@ -97,29 +90,31 @@ app.post("/api/events/:eventId", (req, res) => {
     return;
   }
   const eventId = Number(req.params.eventId);
-  
   db.run(`
     REPLACE INTO EventUsers (eventId, userName, estimatedArrivalTime, 
       lastUpdatedTime) VALUES (${eventId}, "${req.body.userName}", 
       "${req.body.estimatedArrivalTime}", "${req.body.lastUpdatedTime}");
   `);
-
   const r = db.exec(`
     SELECT userName, estimatedArrivalTime, lastUpdatedTime FROM EventUsers
       WHERE eventId = ${eventId} AND userName != "${req.body.userName}"
       ORDER BY userName COLLATE NOCASE ASC;
   `);
-  
   const users = []; 
-
   if (r && r[0] && r[0].values && r[0].values[0] && r[0].values.every((user) => user.length == 3)) {
     r[0].values.forEach((user) => users.push({userName: user[0], estimatedArrivalTime: user[1], lastUpdatedTime: user[2]}));
   }
-
   logResult(req, res, users);
+}
 
-});
+function perror(req, res, msg, ecode=500) {
+  console.error("%s %s %s", req.method, req.originalUrl, msg);
+  res.sendStatus(ecode)
+}
 
-app.listen(app.get("port"));
-console.log("Running localhost:%s/api", app.get("port"))
+function logResult(req, res, resMap) {
+  console.log("%s %s -> %s", req.method, req.originalUrl, JSON.stringify(resMap));
+  res.json(resMap);
+}
+
 
