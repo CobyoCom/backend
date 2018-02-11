13ab5e4260e039f76f84c9c12d4f7945904b148b
asdf <a@b.c>
2018-02-11 14:39:34 -0500

    logging

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo kill $(ps aux | grep '[n]ode server.js' | awk '{print $2}')
+sudo kill $(ps aux | grep '[n]ode server.js' | awk '{print $2}') &> /dev/null
 NODE_ENV=production node server.js &> log/server.log &
 
