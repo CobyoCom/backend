944112379d83a6747d2ae9e16cb5b1d7d2745e35
Seob Whang <alexwhang94@gmail.com>
2018-04-29 16:50:32 -0400

    Graphql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 const AWS = require("aws-sdk");
+const graphql = require('graphql');
 const db = new AWS.DynamoDB.DocumentClient({endpoint: process.env.DYNAMODB_ENDPOINT, region: process.env.AWS_REGION}); 
 const headers = {  
   "access-control-allow-headers": "content-type",
@@ -7,13 +8,73 @@ const headers = {
   "access-control-allow-origin": process.env.ALLOW_ORIGIN
 };
 
-exports.handler = function(event, context, callback) {
-  const ret = (code, js) => {
-    if (code >= 400) console.error((js.message = "ERROR " + event.httpMethod + " " + event.path +  " " + (event.body || "(no body)") + ": " + code + ", " + (js.message || "(no message)")));
-    callback(null, {statusCode: code, headers: headers, body: JSON.stringify(js)});
+const schema = graphql.buildSchema(`
+  type Event {
+    eventId: Int! // Unique but not necessarily auto-increment
+    eventUsers: [EventUser]
+    notifications: [Notification]
   }
-  var body = {}, params, query = event.queryStringParameters;
-  if (event.body) try {body = JSON.parse(event.body);} catch (error) {ret(400, {message: "Body cannot be parsed as JSON"}); return;}
+  type User {
+    name: String! // Unique
+    userEvents: [EventUser]
+  }
+  type EventUser {
+    event: Event!
+    user: User!
+    duration: Int
+    updatedAt: Int
+    travelMode: String
+    hasLeft: Boolean
+  }
+  type Notification {
+    notificationId: Int!
+    createdAt: Int!
+    message: String!
+    reactions: [Reaction]
+  }
+  type Reaction {
+    user: User!
+    emoji: String!
+  }
+  type Query {
+    event(eventId: Int!): Event
+    user(userName: String!): User
+  }
+  type Mutation {
+    createEvent(placeId: String!): Event
+    updateEventUser(eventId: Int!, userName: String!, duration: Int, lastUpdated: Int, travelMode: String, hasLeft: Boolean): EventUser
+    createEventUser(eventId: Int!, userName: String!): EventUser
+    createReaction(notificationId: Int!, userName: String!, emoji: String!): Reaction
+    deleteReaction(notificationId: Int!, userName: String!, emoji: String!): Boolean
+  }
+`);
+
+const root = {
+  getEvent: (args, b, c) => {
+    return Event.find(args.id);
+  }, 
+  createEvent: (args, b, c) => {
+    return new Event(args.placeId);
+  }
+}
+
+class Event {
+  constructor() {
+    this.id = id;
+  }
+}
+
+class User {
+}
+
+class EventUser {
+}
+
+class Notification {
+}
+
+class Reaction {
+}
 
   const put = () => {
     body.id = Math.floor(Math.random() * 10000).toString();
@@ -63,108 +124,24 @@ exports.handler = function(event, context, callback) {
     db.query({TableName: process.env.TABLE_USER, KeyConditionExpression: "eventId = :1", ExpressionAttributeValues: {":1": params[1]}}, (err, data) => {
       (err)? ret(err.statusCode || 500, err): ret(200, data.Items)
     });
-  else if ((params = ptr("/api/log", event.path)) && event.httpMethod == "POST") {
-    console.error("ERROR Client: " + (event.body || "(no body)"));
-    ret(200, {});
-  } else if (event.httpMethod == "OPTIONS") {
-    ret(200, {});
-  } else ret(400, {message: "URL is not a valid endpoint"});
-}
 
-function ptr(str, matchingPath) {
-  const escapeString = (str) => str.replace(/([.+*?=^!:${}()[\]|/\\])/g, '\\$1');
-  var DEFAULT_DELIMITER = '/'
-  var DEFAULT_DELIMITERS = './'
-  var PATH_REGEXP = new RegExp(['(\\\\.)', '(?:\\:(\\w+)(?:\\(((?:\\\\.|[^\\\\()])+)\\))?|\\(((?:\\\\.|[^\\\\()])+)\\))([+*?])?'].join('|'), 'g')
-  var tokens = []
-  var key = 0
-  var index = 0
-  var path = ''
-  var defaultDelimiter = DEFAULT_DELIMITER
-  var delimiters = DEFAULT_DELIMITERS
-  var pathEscaped = false
-  var res
-  while ((res = PATH_REGEXP.exec(str)) !== null) {
-    var m = res[0]
-    var escaped = res[1]
-    var offset = res.index
-    path += str.slice(index, offset)
-    index = offset + m.length
-    if (escaped) {
-      path += escaped[1]
-      pathEscaped = true
-      continue
-    }
-    var prev = ''
-    var next = str[index]
-    var name = res[2]
-    var capture = res[3]
-    var group = res[4]
-    var modifier = res[5]
-    if (!pathEscaped && path.length) {
-      var k = path.length - 1
-      if (delimiters.indexOf(path[k]) > -1) {
-        prev = path[k]
-        path = path.slice(0, k)
-      }
-    }
-    if (path) {
-      tokens.push(path)
-      path = ''
-      pathEscaped = false
-    }
-    var partial = prev !== '' && next !== undefined && next !== prev
-    var repeat = modifier === '+' || modifier === '*'
-    var optional = modifier === '?' || modifier === '*'
-    var delimiter = prev || defaultDelimiter
-    var pattern = capture || group
-    tokens.push({
-      name: name || key++,
-      prefix: prev,
-      delimiter: delimiter,
-      optional: optional,
-      repeat: repeat,
-      partial: partial,
-      pattern: pattern ? pattern.replace(/([=!:$/()])/g, '\\$1') : '[^' + escapeString(delimiter) + ']+?'
-    })
+
+exports.handler = function(event, context, callback) { 
+  try {
+    var code = 200, ret = {};
+    const body = JSON.parse(event.body);
+    if (event.path == "OPTIONS") {}
+    if (event.path == "/graphql") graphql.graphql(schema, body.query, root, null, body.variables, body.operationName, null).then((response) => {
+      if (response.errors) throw JSON.stringify(response.errors);
+      ret = response;
+    });
+    else if (event.path == "/log") console.error("CLINET ERROR: " + event.body);
+    else throw "URL is invalid enpoint";
+  } catch (error) {
+    code = 400;
+    ret = {message: error.message};
+    console.error("ERROR " + event.httpMethod + " " + event.path +  " " + (event.body || "(no body)") + ": 400, " + error.message);
+  } finally {
+    callback(null, {statusCode: code, headers: headers, body: JSON.stringify(ret)});
   }
-  if (path || index < str.length) {
-    tokens.push(path + str.substr(index))
-  }
-  var strict = null;
-  var end = true;
-  var delimiter = escapeString(DEFAULT_DELIMITER)
-  var delimiters = DEFAULT_DELIMITERS
-  var endsWith = [].map(escapeString).concat('$').join('|')
-  var route = ''
-  var isEndDelimited = false
-  for (var i = 0; i < tokens.length; i++) {
-    var token = tokens[i]
-    if (typeof token === 'string') {
-      route += escapeString(token)
-      isEndDelimited = i === tokens.length - 1 && delimiters.indexOf(token[token.length - 1]) > -1
-    } else {
-      var prefix = escapeString(token.prefix)
-      var capture = token.repeat
-        ? '(?:' + token.pattern + ')(?:' + prefix + '(?:' + token.pattern + '))*'
-        : token.pattern
-      if (token.optional) {
-        if (token.partial) {
-          route += prefix + '(' + capture + ')?'
-        } else {
-          route += '(?:' + prefix + '(' + capture + '))?'
-        }
-      } else {
-        route += prefix + '(' + capture + ')'
-      }
-    }
-  }
-  if (end) {
-    if (!strict) route += '(?:' + delimiter + ')?'
-    route += endsWith === '$' ? '$' : '(?=' + endsWith + ')'
-  } else {
-    if (!strict) route += '(?:' + delimiter + '(?=' + endsWith + '))?'
-    if (!isEndDelimited) route += '(?=' + delimiter + '|' + endsWith + ')'
-  }
-  return (new RegExp('^' + route, '')).exec(matchingPath);
 }
