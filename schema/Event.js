e09d7fa75ddbab197d0bd238084400313c18afb4
Seob Whang <alexwhang94@gmail.com>
2018-07-28 00:50:29 +0000

    Reimplemented endEvent

--- schema/Event.js
+++ schema/Event.js
@@ -9,6 +9,7 @@ const type = new GraphQLObjectType({
       eventId: { type: new GraphQLNonNull(GraphQLString) },
       placeId: { type: new GraphQLNonNull(GraphQLString) },
       eventName: { type: new GraphQLNonNull(GraphQLString) },
+      dateEnded: { type: GraphQLString },
       eventUsers: require("./EventUser").fieldForEvent,
       notifications: require("./Notification").fieldForEvent
     };
@@ -16,12 +17,15 @@ const type = new GraphQLObjectType({
 });
 
 module.exports.build = function({query, mutation}) {
- query.event = { 
+ query.event = {
     type: type,
     args: { eventId: { type: new GraphQLNonNull(GraphQLString) } },
     resolve: function(_, {eventId}, {db, Events}) {
       return new Promise(function (resolve, reject) {
-        db.get({TableName: Events, Key: {eventId}}, function (err, data) {
+        db.get({
+          TableName: Events,
+          Key: { eventId }
+        }, function (err, data) {
           if (err) return reject(err.message);
           if (!data.Item) return reject("eventId " + eventId + " doesn't exist on DB");
           return resolve(data.Item);
@@ -29,23 +33,47 @@ module.exports.build = function({query, mutation}) {
       });
     }
   };
-  
+
   mutation.createEvent = {
     type: type,
-    args: { 
+    args: {
       placeId: { type: new GraphQLNonNull(GraphQLString) },
       eventName: { type: new GraphQLNonNull(GraphQLString) },
     },
     resolve: function(_, {placeId, eventName}, {db, Events}) {
       function put(resolve, reject) {
         const eventId = Math.floor(Math.random() * 10000).toString();
-        db.put({TableName: Events, Item: {eventId, placeId, eventName, eventUsers: [], notifications: []}, ConditionExpression: "attribute_not_exists(eventId)"}, function (err, data) {
+        db.put({
+          TableName: Events,
+          Item: {eventId, placeId, eventName, eventUsers: [], notifications: []},
+          ConditionExpression: "attribute_not_exists(eventId)"
+        }, function (err, data) {
           if (err && err.code == "ConditionalCheckFailedException") return put(resolve, reject);
           if (err) return reject(err.message);
           return resolve({eventId, placeId, eventName});
         });
       }
       return new Promise(put);
+    }
+  };
+
+  mutation.endEvent = {
+    type: type,
+    args: { eventId: { type: new GraphQLNonNull(GraphQLString) } },
+    resolve: function(_, {eventId}, {db, Events}) {
+      return new Promise(function(resolve, reject) {
+        db.update({
+          TableName: Events,
+          Key: { eventId },
+          UpdateExpression: "SET #a = :x",
+          ExpressionAttributeNames: { "#a": "dateEnded" },
+          ExpressionAttributeValues: { ":x" : (new Date()).getTime() },
+          ReturnValues: 'ALL_NEW'
+        }, (err, data) => {
+          if (err) return reject(err.message);
+          return resolve(data.Attributes);
+        });
+      });
     }
   };
 }
