5174eb55251033506bf6218a027d621f1e5ecbc8
Seob Whang <alexwhang94@gmail.com>
2020-04-05 03:03:13 +0000

    Add back autodelete dynamodb

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker run -d --name dynamodb -p 9520:8000 amazon/dynamodb-local
-AWS_REGION=foobar AWS_ACCESS_KEY_ID=foobar AWS_SECRET_ACCESS_KEY=foobar aws --endpoint-url http://localhost:9520 dynamodb create-table --table-name Table --attribute-definitions AttributeName=userId,AttributeType=S AttributeName=groupId,AttributeType=S --key-schema AttributeName=userId,KeyType=HASH AttributeName=groupId,KeyType=RANGE --global-secondary-index IndexName=Group,KeySchema='[{AttributeName=groupId,KeyType=HASH},{AttributeName=userId,KeyType=RANGE}]',Projection='{ProjectionType=ALL}',ProvisionedThroughput='{ReadCapacityUnits=1,WriteCapacityUnits=1}' --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
-AWS_REGION=foobar AWS_ACCESS_KEY_ID=foobar AWS_SECRET_ACCESS_KEY=foobar aws --endpoint-url http://localhost:9520 dynamodb put-item --table-name Table --item '{"userId":{"S":"Event-1"},"groupId":{"S":"Event-1"},"code":{"S":"1"},"place":{"M":{"googlePlaceId":{"S":"ChIJ7VHBwnZ644kRKRWP5Qe27v4"}}},"eventName":{"S":"Royale"}}'
-
-rm -rf node_modules package-lock.json
+sudo docker run --rm -p 9520:8000 amazon/dynamodb-local &
+PID=$!
+trap "sudo kill -TERM $PID" TERM INT
+AWS_REGION=foobar AWS_ACCESS_KEY_ID=foobar AWS_SECRET_ACCESS_KEY=foobar aws --endpoint-url http://localhost:9520 dynamodb create-table --table-name Table --attribute-definitions AttributeName=userId,AttributeType=S AttributeName=groupId,AttributeType=S --key-schema AttributeName=userId,KeyType=HASH AttributeName=groupId,KeyType=RANGE --global-secondary-index IndexName=Group,KeySchema='[{AttributeName=groupId,KeyType=HASH},{AttributeName=userId,KeyType=RANGE}]',Projection='{ProjectionType=ALL}',ProvisionedThroughput='{ReadCapacityUnits=1,WriteCapacityUnits=1}' --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 >/dev/null
+AWS_REGION=foobar AWS_ACCESS_KEY_ID=foobar AWS_SECRET_ACCESS_KEY=foobar aws --endpoint-url http://localhost:9520 dynamodb put-item --table-name Table --item '{"userId":{"S":"Event-1"},"groupId":{"S":"Event-1"},"code":{"S":"1"},"place":{"M":{"googlePlaceId":{"S":"ChIJ7VHBwnZ644kRKRWP5Qe27v4"}}},"eventName":{"S":"Royale"}}' > /dev/null
+# rm -rf node_modules package-lock.json
 npm install graphql fs uuid https web-push cryptr express aws-sdk
 API_PATH=/graphql DEV_API_PORT=3001 ALLOW_ORIGIN=http://localhost:3000 AWS_REGION=foobar AWS_ACCESS_KEY_ID=foobar AWS_SECRET_ACCESS_KEY=foobar DYNAMODB_ENDPOINT=http://localhost:9520 TABLE_NAME=Table SESSION_KEY=dev node dev_server.js
